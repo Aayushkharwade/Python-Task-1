d2ecb0190950707fadd1ed38fb2db42b8ce03a97
Aayushkharwade <107250132+Aayushkharwade@users.noreply.github.com>
2023-02-24 00:16:30 +0530

    Add files via upload

---
+++ dextop_notifier.py
@@ -0,0 +1,13 @@
+import time
+from plyer import notification
+ 
+if __name__ == '__main__':
+
+        notification.notify(
+ 			title = "* * * Python Dextop Notifier  * * *",
+ 			message ="SYNC INTERNS PYTHON PROJECT",
+ 			app_icon = "D:\Python Programs Vs Code\SYNC INTERNS PROJECT\sync intern icon.ico",
+ 			timeout= 12
+ 			)
+ 	 
+      
